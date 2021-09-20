b16e92446afe61b279b100aa25356b8520d3f53c
reza delbaz <reza.delbaz74@gmail.com>
2021-09-21 00:04:09 +0430

    update Comment Component

--- src/container/Discussion/Discussion.js
+++ src/container/Discussion/Discussion.js
@@ -1,14 +1,31 @@
+import { useEffect, useState } from "react";
 import Comment from "../../components/comment/Comment";
 import FullComment from "../../components/FullComment/fullComment";
 import NewComment from "../../components/NewComment/NewComment";
 import "./Discussion.css";
+import axios from "axios";
 const Discussion = () => {
+  const [comments, setComment] = useState(null);
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/comments")
+      .then((response) => {
+        setComment(response.data.slice(0, 4));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
     <main>
       <section>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments ? (
+          comments.map((c) => (
+            <Comment key={c.id} name={c.name} email={c.email} />
+          ))
+        ) : (
+          <p>comments Loading...</p>
+        )}
       </section>
       <section>
         <FullComment />
